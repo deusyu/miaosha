4fdfcae1f8bc8f3139fdbfa818638d2a57f16aca
Daniel <dongyuchiao@gmail.com>
2021-08-04 18:20:43 +0800

    Chapter10

--- src/main/java/com/miaoshaproject/controller/OrderController.java
+++ src/main/java/com/miaoshaproject/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.miaoshaproject.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.miaoshaproject.error.BusinessException;
 import com.miaoshaproject.error.EmBusinessError;
 import com.miaoshaproject.mq.MqProducer;
@@ -10,6 +11,7 @@ import com.miaoshaproject.service.PromoService;
 import com.miaoshaproject.service.model.OrderModel;
 import com.miaoshaproject.service.model.UserModel;
 
+import com.miaoshaproject.util.CodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
@@ -17,7 +19,14 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.RenderedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
 import java.util.concurrent.*;
 
 @Controller("order")
@@ -41,16 +50,46 @@ public class OrderController extends BaseController{
     private PromoService promoService;
 
     private ExecutorService executorService;
+
+    private RateLimiter orderCreateRateLimiter;
+
     @PostConstruct
     public void init(){
         executorService = Executors.newFixedThreadPool(20);
+
+        //限流技术
+        orderCreateRateLimiter = RateLimiter.create(300);
+
+    }
+
+    //生成验证码
+    @RequestMapping(value = "/generateverifycode",method = {RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public void generateverifycode(HttpServletResponse response) throws BusinessException, IOException {
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if(StringUtils.isEmpty(token)){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登陆，不能生成验证码");
+        }
+        //获取登陆用户的信息
+        UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
+        if(userModel == null ){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登陆，不能生成验证码");
+        }
+        Map<String,Object> map = CodeUtil.generateCodeAndPic();
+        redisTemplate.opsForValue().set("verify_code_"+userModel.getId(),map.get("code"));
+        redisTemplate.expire("verify_code_"+userModel.getId(),10,TimeUnit.MINUTES);
+
+        ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", response.getOutputStream());
+        System.out.println("验证码的值为："+map.get("code"));
+
     }
 
     //生成秒杀令牌
     @RequestMapping(value = "/generatetoken",method = {RequestMethod.POST},consumes = {CONTENT_TYPE_FORMED})
     @ResponseBody
     public CommonReturnType generatetoken(@RequestParam(name = "itemId")Integer itemId,
-                                        @RequestParam(name = "promoId")Integer promoId) throws BusinessException {
+                                          @RequestParam(name = "promoId")Integer promoId,
+                                          @RequestParam(name = "verifyCode")String verifyCode) throws BusinessException {
     //根据token获取用户信息
         String token = httpServletRequest.getParameterMap().get("token")[0];
         if(StringUtils.isEmpty(token)){
@@ -60,6 +99,14 @@ public class OrderController extends BaseController{
         UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
         if(userModel == null ){
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登陆，不能下单");
+        }
+        //通过verifycode验证码的有效性
+        String redisVerifyCode = (String) redisTemplate.opsForValue().get("verify_code_"+userModel.getId());
+        if(StringUtils.isEmpty(redisVerifyCode)){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"请求非法");
+        }
+        if(!redisVerifyCode.equalsIgnoreCase(verifyCode)){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"请求非法，验证码错误");
         }
         //获取秒杀令牌
         String promoToken = promoService.generateSecondKillToken(promoId,itemId,userModel.getId());
@@ -87,6 +134,9 @@ public class OrderController extends BaseController{
 
         //Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
 
+        if(!orderCreateRateLimiter.tryAcquire()){
+            throw new BusinessException(EmBusinessError.RATELIMIT);
+        }
 
         String token = httpServletRequest.getParameterMap().get("token")[0];
         if(StringUtils.isEmpty(token)){
@@ -100,8 +150,9 @@ public class OrderController extends BaseController{
         //校验秒杀令牌是否正确
         if(promoId != null){
             String inRedispromoToken = (String) redisTemplate.opsForValue().get("promo_token_"+promoId+"_userId_"+userModel.getId()+"_itemId_"+itemId);
-            if(inRedispromoToken == null)
-                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"秒杀令牌校验失败");
+            if(inRedispromoToken == null) {
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "秒杀令牌校验失败");
+            }
             if(!org.apache.commons.lang3.StringUtils.equals(promoToken,inRedispromoToken)){
                 throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"秒杀令牌校验失败");
             }
@@ -122,7 +173,7 @@ public class OrderController extends BaseController{
                 String stockLogId = itemService.initStockLog(itemId, amount);
 
 
-                //再去完成对应的下单实物性消息机制
+                //再去完成对应的下单事务性消息机制
                 if (!mqProducer.transactionAsyncReduceStock(userModel.getId(), itemId, promoId, amount, stockLogId)) {
                     throw new BusinessException(EmBusinessError.UNKNOWN_ERROR, "下单失败");
                 }
